bcddd6bce304e0de9e31d7ce5037b0d5bd52fab9
Raito Muramatsu <55012572+raitolaito@users.noreply.github.com>
2020-09-08 10:28:52 +0900

    Add files via upload

--- board.php
+++ board.php
@@ -10,14 +10,16 @@
         <p>名前：<input type="text" name = "name"></p>
         <p>コメント：<br>
             <textarea name="comment" rows="4" cols="40"></textarea> </p>
-        <p><input type ="submit" value="送信" ></p>
+        <p><input type ="submit" value="投稿" ></p>
     </form>
+    
     <?php //テキストファイルを読み込み、下に表示させる
     $posts = file("post.txt");
     foreach($posts as $post): //ループ
         $content = explode("<>",$post); //配列を"<>"でさらに分割
-        echo $content[0]."名前：".$content[1]."　日付：".$content[3]."<br />".$content[2];
+        echo $content[0]."名前：".$content[1]."　日時：".$content[3]."<br />".$content[2];
         echo "<br />";
     endforeach;
     ?>
+    
 </body>
--- form.php
+++ form.php
@@ -1,7 +1,6 @@
 <?php
 $name = $_POST['name'];
 $comment = $_POST['comment'];
-echo "送信完了";
 $contents = file("post.txt");
 if($contents == NULL){
     $number = 0;
@@ -10,7 +9,30 @@ if($contents == NULL){
 }
 
 $number += 1;
-$time = date("Y-m-d");
+$time = date("Y-m-d H:i");
 
 file_put_contents("post.txt",$number."<>". $name."<>".$comment."<>".$time.PHP_EOL, FILE_APPEND);
 ?>
+<head>
+<meta charset = "UFT-8">
+<title>簡易掲示板</title>
+</head>
+<body>
+    <h2>簡易掲示板</h2>
+    <form action = "form.php" method = "post">
+        <p>名前：<input type="text" name = "name"></p>
+        <p>コメント：<br>
+            <textarea name="comment" rows="4" cols="40"></textarea> </p>
+        <p><input type ="submit" value="投稿" ></p>
+    </form>
+    
+    <?php //テキストファイルを読み込み、下に表示させる
+    $posts = file("post.txt");
+    foreach($posts as $post): //ループ
+        $content = explode("<>",$post); //配列を"<>"でさらに分割
+        echo $content[0]."名前：".$content[1]."　日時：".$content[3]."<br />".$content[2];
+        echo "<br />";
+    endforeach;
+    ?>
+    
+</body>
